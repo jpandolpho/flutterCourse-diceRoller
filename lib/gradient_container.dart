4ef8a782da11bb720a203668da59e659a38c67fb
João Pedro Andolpho <joaopedrodealmeidaandolpho@gmail.com>
2024-04-26 17:04:05 -0300

    Finished section 2 until lesson 42. Many changes, but the main application has not been started yet. Only introductory topics have been covered so far.

---
+++ lib/gradient_container.dart
@@ -0,0 +1,28 @@
+import "package:flutter/material.dart";
+import 'package:dice_roller/styled_text.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.deepPurple,
+            Color.fromARGB(255, 65, 30, 125),
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StyledText("Hello World!"),
+      ),
+    );
+  }
+}
