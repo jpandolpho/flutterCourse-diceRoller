9121d8dddda9342b2f9022f33cb6d8c4b61d0deb
João Pedro Andolpho <joaopedrodealmeidaandolpho@gmail.com>
2024-04-29 15:23:21 -0300

    Finished lesson 46, the button looks less bad but it's still just text. If the course doesn't turn it into an actual button, I might change it on my own.

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -22,13 +22,24 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
               'assets/images/dice-2.png',
               width: 200,
             ),
-            TextButton(onPressed: rollDice, child: const Text('Roll Dice'))
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(top: 20),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              child: const Text('Roll Dice'),
+            )
           ],
         ),
       ),
