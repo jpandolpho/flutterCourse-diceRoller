d7adc4a5c643960874c23c68adb0b0402fa7b957
João Pedro Andolpho <joaopedrodealmeidaandolpho@gmail.com>
2024-04-29 14:55:19 -0300

    Finished lesson 43 and added the images from lesson 44 to the project and to pubspec.yaml

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -5,17 +5,16 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Colors.deepPurple,
-            Color.fromARGB(255, 65, 30, 125),
-          ],
+          colors: colors,
           begin: startAlignment,
           end: endAlignment,
         ),
@@ -26,3 +25,30 @@ class GradientContainer extends StatelessWidget {
     );
   }
 }
+
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer(Color this.color1, Color this.color2, {super.key});
+
+//   final Color color1;
+//   final Color color2;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: [
+//             color1,
+//             color2,
+//           ],
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText("Hello World!"),
+//       ),
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,8 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(
+            colors: [Colors.deepPurple, Color.fromARGB(255, 65, 30, 125)]),
       ),
     ),
   );
