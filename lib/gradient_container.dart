a024e682c1179dda1b2f97d570fd8f8fa9a77071
João Pedro Andolpho <joaopedrodealmeidaandolpho@gmail.com>
2024-04-29 15:02:05 -0300

    Finished lesson 44, removing the text from the app and adding the image

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,4 @@
 import "package:flutter/material.dart";
-import 'package:dice_roller/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
@@ -19,8 +18,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText("Hello World!"),
+      child: Center(
+        child: Image.asset('assets/images/dice-2.png', width: 200),
       ),
     );
   }
